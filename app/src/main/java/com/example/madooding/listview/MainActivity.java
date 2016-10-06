25e0428fcd97284761683b80bf5a06512bd09cf2
s7070112@kmitl.ac.th <s7070112@kmitl.ac.th>
2016-10-06 16:49:51 +0700

    Delete some line for more readable

--- app/src/main/java/com/example/madooding/listview/MainActivity.java
+++ app/src/main/java/com/example/madooding/listview/MainActivity.java
@@ -5,13 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 import com.example.madooding.listview.adapter.CustomAdapter;
 import java.util.ArrayList;
 import java.util.List;
-import com.example.madooding.listview.RequestCode;
 
 public class MainActivity extends AppCompatActivity {
 
